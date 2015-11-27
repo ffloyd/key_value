fdaa0ba9c3493b7e56d1fb835d60f75923e04e3b
Roman Kolesnev <rvkolesnev@gmail.com>
2015-11-27 18:30:05 +0300

    Add Store.Bucket

---
+++ apps/store/test/store/bucket_test.exs
@@ -0,0 +1,27 @@
+defmodule Store.BucketTest do
+  use ExUnit.Case
+
+  setup do
+    {:ok, bucket} = Store.Bucket.start_link
+    {:ok, bucket: bucket}
+  end
+
+  test "get: return nil when no value for key", %{bucket: bucket} do
+    assert Store.Bucket.get(bucket, "key") == nil
+  end
+
+  test "get, put: saves values and returns it", %{bucket: bucket} do
+    assert Store.Bucket.put(bucket, "key", "value") == :ok
+    assert Store.Bucket.get(bucket, "key") == "value"
+  end
+
+  test "delete: deletes value by key", %{bucket: bucket} do
+    Store.Bucket.put(bucket, "key", "value")
+    assert Store.Bucket.delete(bucket, "key") == :ok
+    assert Store.Bucket.get(bucket, "key") == nil
+  end
+
+  test "delete: return :ok even if key doesn't exists", %{bucket: bucket} do
+    assert Store.Bucket.delete(bucket, "key") == :ok
+  end
+end
--- apps/store/test/store_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule StoreTest do
-  use ExUnit.Case
-  doctest Store
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
